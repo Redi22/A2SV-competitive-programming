e43e756a16ff7409f6f6cc5dea3fc067e397914e
Redi22 <redidemisse22@gmail.com>
2021-12-13 18:37:00 +0300

    selection sort

---
+++ Week 1/selection_sort.cpp
@@ -0,0 +1,24 @@
+  class Solution
+{
+  
+  public:
+    int select(int arr[], int i)
+    {
+        // code here such that selectionSort() sorts arr[]
+    }
+     
+    void selectionSort(int arr[], int n)
+    {
+       //code here
+        for(int i = 0; i < n - 1; i++) {
+            int min = i;
+            for(int j = i + 1; j < n; j++) {
+                if(arr[j] < arr[min])
+                min = j;
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+            
+        }
+    } 
