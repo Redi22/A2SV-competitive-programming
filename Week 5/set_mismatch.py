e43a0426220c37a740df429950e92fb4835c2276
Redi22 <redidemisse22@gmail.com>
2022-01-10 01:14:33 +0300

    set-mismatch

---
+++ Week 5/set_mismatch.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        mismatch = []
+        for i in range(1 , len(nums)+1):
+            nums.append(i)
+        nums = Counter(nums)
+        for num , freq in nums.items():
+            if(freq != 2):
+                mismatch.append(num)
+        return mismatch
