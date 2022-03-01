babc66b796693e589c1671c55c209684081080fd
Redi22 <redidemisse22@gmail.com>
2022-03-01 12:19:26 +0300

    invert tree

---
+++ CAMP/invert_tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root: 
+            return
+        
+        left = root.left
+        right = root.right
+        
+        root.left = self.invertTree(right)
+        root.right = self.invertTree(left)
+        
+        return root
