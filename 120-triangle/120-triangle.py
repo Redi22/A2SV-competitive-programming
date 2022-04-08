1ef215b05289f6503ffea143dea08f13260af7f2
Redi_D <45771674+Redi22@users.noreply.github.com>
2022-04-08 18:27:28 +0300

    Time: 101 ms (44.56%), Space: 17.2 MB (10.01%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -3,22 +3,22 @@ class Solution:
         
         #Top-down
         
-#         memo = {}
-#         def dfs(i , j):
-#             if i == len(triangle):
-#                 return 0
-#             if (i,j) in memo.keys():
-#                 return memo[(i,j)]
-#             memo[(i,j)] = triangle[i][j] + min(dfs(i+1 , j) , dfs(i+1 , j+1))
-#             return memo[(i,j)]
-#         return triangle[0][0] + min(dfs(1,0) , dfs(1,1)) 
+        memo = {}
+        def dfs(i , j):
+            if i == len(triangle):
+                return 0
+            if (i,j) in memo.keys():
+                return memo[(i,j)]
+            memo[(i,j)] = triangle[i][j] + min(dfs(i+1 , j) , dfs(i+1 , j+1))
+            return memo[(i,j)]
+        return triangle[0][0] + min(dfs(1,0) , dfs(1,1)) 
     
     
-#     #bottom-up 
+    #bottom-up 
     
-        for i in range(len(triangle)-2 , -1 ,-1):
-            for j in range(len(triangle[i])):
-                triangle[i][j] += min(triangle[i+1][j] , triangle[i+1][j+1]) 
+#         for i in range(len(triangle)-2 , -1 ,-1):
+#             for j in range(len(triangle[i])):
+#                 triangle[i][j] += min(triangle[i+1][j] , triangle[i+1][j+1]) 
                 
-        return triangle[0][0]
+#         return triangle[0][0]
         
