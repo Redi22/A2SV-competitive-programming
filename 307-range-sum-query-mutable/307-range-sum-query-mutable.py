8ae02591e17af9d6307e10479d9cbf31e6b0cfc6
Redi_D <45771674+Redi22@users.noreply.github.com>
2022-07-31 21:07:41 +0300

    Time: 1609 ms (93.75%), Space: 31.4 MB (60.48%) - LeetHub

---
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.py
@@ -0,0 +1,26 @@
+class NumArray:
+    nums = []
+    s = 0
+    l = 0
+    
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.s = sum(nums)
+        self.len = len(nums)
+
+    def update(self, index: int, val: int) -> None:
+        self.s -= self.nums[index]
+        self.nums[index] = val
+        self.s += self.nums[index]
+
+    def sumRange(self, left: int, right: int) -> int:
+        if right - left > self.len // 2:
+            ans = sum(self.nums[:left]) + sum(self.nums[right + 1:])
+            return self.s - ans
+        else:
+            return sum(self.nums[left: right + 1])
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# obj.update(index,val)
+# param_2 = obj.sumRange(left,right)
