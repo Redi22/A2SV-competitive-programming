bed1e5ab67f02c65d963f4e07d97fbfd925d151f
Redi_D <45771674+Redi22@users.noreply.github.com>
2022-10-09 15:51:31 +0300

    Time: 112 ms (36.24%), Space: 14.1 MB (10.20%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        res = []
+        cols = set()
+        negDiag = set()
+        posDiag = set()
+        board = [["."] * n for i in range(n)]
+        
+        def bk(i):
+            if i == n:
+                possible = ["".join(row) for row in board]
+                res.append(possible)
+                return 
+            
+            for j in range(n):
+                if j in cols or (i - j) in negDiag  or (j + i) in posDiag:
+                    continue
+                    
+                cols.add(j)
+                negDiag.add(i - j)
+                posDiag.add(i + j)
+                board[i][j] = "Q"
+
+                bk(i + 1)
+
+                cols.remove(j)
+                negDiag.remove(i - j)
+                posDiag.remove(i + j)
+                board[i][j] = "."
+
+            
+        bk(0)
+        return len(res)
