41ac1c2fa364c3dd2f00609b49eb87a65504a292
Redi_D <45771674+Redi22@users.noreply.github.com>
2022-10-11 21:22:53 +0300

    Time: 672 ms (52.12%), Space: 18 MB (70.48%) - LeetHub

--- 0687-longest-univalue-path/0687-longest-univalue-path.py
+++ 0687-longest-univalue-path/0687-longest-univalue-path.py
@@ -11,17 +11,16 @@ class Solution:
         ans = 0
         def traverse(root, parent):
             nonlocal ans
-            if not root:
-                return 0
+            if not root: return 0
             
             left = traverse(root.left, root.val)
             right = traverse(root.right, root.val)
             ans = max(ans, left + right)
+            
             if root.val == parent:
                 return max(left, right) + 1
+            
             return 0
             
-            
-        
         traverse(root,  root.val)
         return ans
