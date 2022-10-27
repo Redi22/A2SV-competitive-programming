ee12148d8797aa2166a89ce1e54aa2173f0ba67c
Redi_D <45771674+Redi22@users.noreply.github.com>
2022-10-27 13:02:50 +0300

    Time: 65 ms (87.26%), Space: 14.4 MB (92.39%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -0,0 +1,36 @@
+class Solution:
+    def find(self, node, parent):
+            current = node
+            while parent[current] > 0:
+                current = parent[current]
+                
+            return current
+        
+    def union(self, node1, node2, parent):
+        pu = self.find(node1, parent)
+        pv = self.find(node2, parent)
+
+        if pu != -1 and pu != pv:
+            if parent[pu] < parent[pv]:
+                parent[pu] += parent[pv]
+                parent[pv] = pu
+            else:
+                parent[pv] += parent[pu]
+                parent[pu] = pv
+            return True
+        
+        return False 
+        
+    
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        parent = [-1] * (len(edges) + 1)
+        answer = [-1, -1]
+        
+        for frm, to in edges:
+            if not self.union(frm, to, parent):
+                answer = [frm, to]
+                
+        # print(parent)
+        return answer
+                
+        
