34569536295ebeec4315bfae63f7491aaa1a05ae
Redi_D <45771674+Redi22@users.noreply.github.com>
2022-11-02 16:11:09 +0300

    Time: 260 ms (7.80%), Space: 14.6 MB (84.08%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -0,0 +1,37 @@
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        '''
+        count the letters
+        vary from 1 to 2 ** count - 1
+        apply the bits to the letters only 
+        set bit means swap 
+        
+        
+        '''
+        count = len([si for si in s if not si.isdigit()])
+        limit = (2 ** count) - 1
+        variants = []
+        
+        for i in range(limit + 1):
+            result = list(s)
+            shift = 0
+            variant = i
+            
+            for j in range(len(s) - 1, -1, -1):
+                if not result[j].isdigit():
+                    result[j] = result[j].upper() if variant & (1 << shift) else result[j].lower()
+                    shift += 1
+
+                
+            variants.append("".join(result))
+            
+        return variants
+                
+            
+                
+                
+        
+        
+        
+        
+        
