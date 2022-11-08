a50b744f904174406787f207dc589ec723f6ef73
Redi_D <45771674+Redi22@users.noreply.github.com>
2022-11-08 11:14:53 +0300

    Time: 46 ms (77.72%), Space: 13.8 MB (72.02%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.py
+++ 0401-binary-watch/0401-binary-watch.py
@@ -1,28 +1,22 @@
 class Solution:
-    def bk(self, i, valids, turned_on, hr, minute):
-        if turned_on == 0 and hr < 12 and minute < 60:
-            generated_minute = str(minute)
-            if minute < 10:
-                generated_minute = "0" + generated_minute
-                
-            valids.add(str(hr) + ":" + generated_minute)
-            return
-        
-        if turned_on < 0 or i > 9 or hr >= 12 or minute >= 60:
-            return 
-        
-        if i < 4:
-            new_hr = hr | (1 << (3 - i))
-            self.bk(i + 1, valids, turned_on - 1, new_hr, minute)
-            self.bk(i + 1, valids, turned_on, hr, minute)
-        else:
-            new_minute = minute | (1 << (5 - (i - 4)))
-            self.bk(i + 1, valids, turned_on - 1, hr, new_minute)
-            self.bk(i + 1, valids, turned_on, hr, minute)
+    def bitCounter(self, number):
+        count = 0
+        while number:
+            count += (number & 1)
+            number = number >> 1
             
+        return count
             
     def readBinaryWatch(self, turnedOn: int) -> List[str]:
         valids = set()
-        self.bk(0, valids, turnedOn, 0, 0)
+        for hr in range(12):
+            for minute in range(60):
+                
+                if self.bitCounter(hr) + self.bitCounter(minute) == turnedOn:
+                    generated_minute = str(minute)
+                    if minute < 10:
+                        generated_minute = "0" + generated_minute
+
+                    valids.add(str(hr) + ":" + generated_minute)
         
-        return sorted(valids)
+        return valids
