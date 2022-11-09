ab7501494029e4897c391f3d8ea9698600a3a959
Redi_D <45771674+Redi22@users.noreply.github.com>
2022-11-09 14:31:25 +0300

    Time: 198 ms (9.83%), Space: 14.7 MB (59.91%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.py
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -8,17 +8,17 @@ class Solution:
         
         
         '''
-        count = len([si for si in s if not si.isdigit()])
+        count = len([si for si in s if si.isalpha()])
         limit = (2 ** count) - 1
         variants = []
         
         for i in range(limit + 1):
             result = list(s)
             shift = 0
-            variant = i
+            variant = i 
             
             for j in range(len(s) - 1, -1, -1):
-                if not result[j].isdigit():
+                if result[j].isalpha():
                     result[j] = result[j].upper() if variant & (1 << shift) else result[j].lower()
                     shift += 1
 
