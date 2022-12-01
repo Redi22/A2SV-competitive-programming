f834767606554d6ea80fa22fb6bf75f7c7e7af03
Redi_D <45771674+Redi22@users.noreply.github.com>
2022-12-01 17:21:40 +0300

    Time: 4180 ms (58.25%), Space: 49.1 MB (96.64%) - LeetHub

--- 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.py
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.py
@@ -2,22 +2,24 @@ class Solution:
     def productQueries(self, n: int, queries: List[List[int]]) -> List[int]:
         powers = []
         result = []
+        mod = 10**9 + 7
         i = 0
         while n:
             if n & 1:
                 powers.append(2 ** i)
             n = n >> 1
             i += 1
-            
-        for i in range(1, len(powers)):
-            powers[i] *= powers[i - 1] 
-            
-            
+        
+        @cache
+        def dp(start, end):
+            if start > end:
+                return 1
+            if start == end:
+                return powers[start]
+            return (dp(start+1, end-1) * powers[start] * powers[end]) % mod
+        
         for start, end in queries:
-            if start == 0:
-                result.append((powers[end])  % (10 ** 9 + 7))
-            else:
-                result.append((powers[end] // powers[start - 1]) % (10 ** 9 + 7))
+            result.append(dp(start, end))
             
         return result
                 
