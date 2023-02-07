8488f95c05cc45890eb03e3685df38b2cce338f7
Redi_D <45771674+Redi22@users.noreply.github.com>
2023-02-07 23:11:57 +0300

    Time: 831 ms (94.29%), Space: 20 MB (80.77%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,23 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        # Hash map 'basket' to store the types of fruits.
+        basket = {}
+        left = 0
+        
+        # Add fruit from the right index (right) of the window.
+        for right, fruit in enumerate(fruits):
+            basket[fruit] = basket.get(fruit, 0) + 1
+
+            # If the current window has more than 2 types of fruit,
+            # we remove one fruit from the left index (left) of the window.
+            if len(basket) > 2:
+                basket[fruits[left]] -= 1
+
+                # If the number of fruits[left] is 0, remove it from the basket.
+                if basket[fruits[left]] == 0:
+                    del basket[fruits[left]]
+                left += 1
+        
+        # Once we finish the iteration, the indexes left and right 
+        # stands for the longest valid subarray we encountered.
+        return right - left + 1
